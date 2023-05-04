786579a91b3c33cbd0376d8e9bd06e3e7fe0a9db
Reuben <reuben13.nz@gmail.com>
2023-05-04 23:48:15 +1200

    yep

--- SENGProject/src/sengproject/Item.java
+++ SENGProject/src/sengproject/Item.java
@@ -1,5 +1,37 @@
 package sengproject;
 
+
 public class Item {
+	/**
+	 * item
+	 */
+	
+	private int price;
+	private int statValue;
+	private String property;
+	
+	public String getProperty() {
+		return property;
+	}
+	
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public int getStatValue() {
+		return statValue;
+	}
+
+	public void setStatValue(int statValue) {
+		this.statValue = statValue;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
 }
