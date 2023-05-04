24371df2dc6bd4668f56fa63e2cff70ca05d5ff6
rsc103 <rsc103@cs19109bs.canterbury.ac.nz>
2023-05-05 00:45:12 +1200

    market draft

--- SENGProject/src/sengproject/Market.java
+++ SENGProject/src/sengproject/Market.java
@@ -1,5 +1,48 @@
 package sengproject;
 
+import java.util.List;
+
 public class Market {
+	
+	private List<Item> availableItems;
+	private List<Athlete> freeAgents;
+	
+
+
+	public void setAvailableItems(List<Item> availableItems) {
+		this.availableItems = availableItems;
+	}
+	
+	public List<Item> viewAvailableItems() {
+		
+		return availableItems;
+	}
+
+	public List<Athlete> getFreeAgents() {
+		return freeAgents;
+	}
+
+
+	public void setFreeAgents(List<Athlete> freeAgents) {
+		this.freeAgents = freeAgents;
+	}
+	
+	public void buyReserve(newAthlete Athlete, oldAthlete Athlete) {
+		
+		if (Team.reserves.length() == 5) {
+			
+		}
+		Team.addNewAthlete(newAthlete, oldAthlete);
+		
+	}
+	public void buyStarter(newAthlete Athlete, oldAthlete Athlete) {
+		
+		Team.addNewAthlete(newAthlete, oldAthlete);
+		Team.subAthlete
+	}
+
+
+	
+	
 
 }
