9e554e004fccd59a81e3448e2726642e2ad4a539
ist46 <ist46@cs18261kq.canterbury.ac.nz>
2023-05-09 15:51:34 +1200

    item test

--- SENGProject/src/test/ItemTest.java
+++ SENGProject/src/test/ItemTest.java
@@ -3,11 +3,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import sengproject.Item;
+
 class ItemTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void proteinShakeTest() {
+		Item proteinShake = Item.PROTEIN_SHAKE; 
+		int statBoost = proteinShake.getStatBoost();
+		int price = proteinShake.getPrice();
+		String stat = proteinShake.getStat();
+		assertEquals(3, price);
+		assertEquals(5, statBoost);
+		assertEquals("Defence", stat);
+	}
+	
+	@Test
+	public void energyDrinkTest() {
+		Item energyDrink = Item.ENERGY_DRINK;
+		int price = energyDrink.getPrice();
+		int statBoost = energyDrink.getStatBoost();
+		String stat = energyDrink.getStat();
+		assertEquals(5, price);
+		assertEquals(10, statBoost);
+		assertEquals("Stamina", stat);
+	}
+	 
+	@Test
+	public void kneeSleeveTest() {
+		Item kneeSleeve = Item.KNEE_SLEEVE;
+		int price = kneeSleeve.getPrice();
+		int statBoost = kneeSleeve.getStatBoost();
+		String stat = kneeSleeve.getStat();
+		assertEquals(4, price);
+		assertEquals(5, statBoost);
+		assertEquals("Offence", stat);
 	}
 
 }
