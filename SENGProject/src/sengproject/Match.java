e623a48bd94e9c6da9b0bdf92118111d876ed8f5
aaronmac <aaronmac@localhost>
2023-05-09 12:14:17 +1200

    match

--- SENGProject/src/sengproject/Match.java
+++ SENGProject/src/sengproject/Match.java
@@ -1,11 +1,80 @@
 package sengproject;
 
+import java.util.ArrayList;
+
+import sengproject.Athlete;
+import sengproject.Stadium;
+import sengproject.Team;
+import sengproject.Club;
 /**
+ * Matches will be generated randomly and will get harder as weeks go on.
+ * In a match, the stat of their position will be compared.
  * 
- * @author aaronmac
+ * 
+ * @author Isaac Steele
  *
  */
 
-public class Match {
-
+public class Match extends Stadium{
+	
+	/**
+	 * Play match
+	 */
+	public String playMatch(Club me, Team opponent) {
+		int myScore = 0;
+		int opponentScore = 0;
+		ArrayList<Athlete> myTeam = me.viewActiveTeam();
+		ArrayList<Athlete> opponentTeam = opponent.viewActiveTeam();
+		for(int i = 0; i < 2 ; i++) {
+			Athlete myDefender = myTeam.get(i);
+			Athlete oppDefender = opponentTeam.get(i);
+			if(myDefender.getDefenceStat() > oppDefender.getDefenceStat()) {
+				myScore += 10;
+			}
+			else if (myDefender.getDefenceStat() < oppDefender.getDefenceStat()) {
+				opponentScore += 10;
+				myDefender.decreaseStamina(5);
+			}
+			else {
+				myScore += 5;
+				opponentScore += 5;
+			}
+			myDefender.decreaseStamina(10);
+			if (myDefender.getStamina() == 0) {
+				myDefender.setInjuryStatus(true);
+			}
+		}
+		for(int i = 2; i < 4; i++) {
+			Athlete myAttacker = myTeam.get(i);
+			Athlete oppAttacker = opponentTeam.get(i);
+			if(myAttacker.getOffenceStat() > oppAttacker.getOffenceStat()) {
+				myScore += 10;
+			}
+			else if (myAttacker.getOffenceStat() < oppAttacker.getOffenceStat()) {
+				opponentScore += 10;
+				myAttacker.decreaseStamina(5);
+			}
+			else {
+				myScore +=5;
+				opponentScore += 5;
+			}
+			myAttacker.decreaseStamina(10);
+			if (myAttacker.getStamina() == 0) {
+				myAttacker.setInjuryStatus(true);
+			}
+		}
+		if(myScore > opponentScore) {
+			return "W";
+		}
+		else if(myScore < opponentScore) {
+			return "L";
+		}
+		else {
+			return "D";
+		}
+		
+		
+	}
+	
 }
+
