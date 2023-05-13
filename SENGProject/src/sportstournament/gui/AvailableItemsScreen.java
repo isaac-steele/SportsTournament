8e3813d2180e50b846d190f65b303966e5138055
ist46 <ist46@cs18237kq.canterbury.ac.nz>
2023-05-13 17:21:05 +1200

    all screens

---
+++ SENGProject/src/sportstournament/gui/AvailableItemsScreen.java
@@ -0,0 +1,43 @@
+package sportstournament.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AvailableItemsScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AvailableItemsScreen window = new AvailableItemsScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AvailableItemsScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ SENGProject/src/sportstournament/gui/TakeByeScreen.java
@@ -0,0 +1,43 @@
+package sportstournament.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TakeByeScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TakeByeScreen window = new TakeByeScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TakeByeScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
